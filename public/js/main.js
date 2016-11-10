5c111b4d4c357a5677082d4016642cfe20c8059f
Negrasus <elnegroleo9@gmail.com>
2016-11-10 01:43:11 -0300

    change the nav for drop down menu on hover, but still not working. commit before try to deploy with database with heorku

--- public/js/main.js
+++ public/js/main.js
@@ -4,19 +4,34 @@ jQuery(document).ready(function ($) {
 	$('.toggle-nav').click(function(){
 		$('.main-nav').toggle(1000);
 	});
-	$('.catalogo-dropdown').click(function(){
-		$('.catalogo-dropdown ul').slideToggle(500);
+	// $('.catalogo-dropdown').click(function(){
+	// 	$('.catalogo-dropdown ul').slideToggle(500);
+	// })
+
+	// variable pasa setear el TimeOut
+	var retardo
+
+	// Fuerza a que se oculte el drop-nav
+	$('.drop-nav').hide()
+
+	// Función mouseover / mouseout
+	$('.drop-container').mouseover(function () {
+		if( $('.drop-nav').is(':hidden') ) {
+
+			clearTimeout(retardo) // permite que el drop-nav no se repliegue al hover sobre el mismo drop-nav
+			$('.drop-nav').slideDown('medium')
+			$('.drop-nav').mouseover(function () {
+				clearTimeout(retardo) // vuelve a setear tiempo 0 pero ahora sobre el mismo drop-nav
+			})
+
+			$(this).mouseout(function () {
+				clearTimeout(retardo)
+				retardo = setTimeout("$('.drop-nav').slideUp('medium')") // permite que al hacer mouse-out del li container el drop-nav se repliegue de vuelta
+			})
+		}
 	})
 })
-// window.onload = function(){
-//
-//   console.log('sin click');
-//   document.querySelector('.flecha').onclick = function(){
-//     console.log('funciona');
-//   };
-//   $('.flecha').click(function(){
-//     console.log('click');
-//     $('#bannerPrueba').toggleClass("main-bannerTog",1000, "easeOutSine");
-//   })
-// };
+
+
+
 })()
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,15 +9,15 @@
   <div style='clear:both'></div>
 
   <nav class="main-nav">
-    <ul>
+    <ul class="first-level-nav">
       <li><a href="{{route('nosotros')}}">Nosotros</a></li>
       {{-- <li><a href="#">Catalago</a></li> --}}
 
-      <li class="catalogo-dropdown">
+      <li class="drop-container">
           <a href="#">
               Catalogo <b class="caret"></b>
           </a>
-          <ul>
+          <ul class="drop-nav">
               <li><a href="{{route('productMoviRehab')}}">Movilidad y Rehabilitación</a></li>
               <li><a href="{{route('productOrtoPro')}}">Ortopedia y Prótesis</a></li>
               <li><a href="{{route('productCiruImpla')}}">Cirugía e Implantes</a></li>
